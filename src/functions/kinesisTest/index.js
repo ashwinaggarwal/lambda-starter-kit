f8afe22cafa5223619d6cf0dd34e68cca4cae241
Ashwin Aggarwal <ashwinaggarwal7@gmail.com>
2018-09-05 13:53:37 +0100

    feat(kinesis): stream logs to a lambda function via kinesis to dump logs to third patry

--- src/functions/kinesisTest/index.js
+++ src/functions/kinesisTest/index.js
@@ -1,12 +1,18 @@
 import { config } from 'dotenv';
 import { lambda } from '../../common/utils/lambda';
+import { log as logger } from '../../common/utils/logger';
+
+const log = logger.bind('[kinesisTest]');
 
 /* Will read env variables as set in config file, should be first thing in the app to be executed */
 config();
 
 export const get = lambda((event, context, callback) => {
+  // process logs here
+
+  log('Event', JSON.stringify(event));
   return callback(null, {
     statusCode: 200,
-    body: 'Welcome to Buckingham Palace'
+    body: 'Healthy and receiving logs'
   });
 });
